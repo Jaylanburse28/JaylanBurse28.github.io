a3304881444bd015f78a64071c15ba40a27d9429
Jaylanburse28 <jaylan.burse@sciacademy.org>
2024-09-09 15:15:11 +0000

    todo 2

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -45,13 +45,14 @@ createPlatform(660,450,200, 10)
 createPlatform(290,300,10,200)
 createPlatform(290,501,30,15)
 createPlatform(290,371,30,15)
+
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+createCollectable('database', 5, 235, 1, 0);
+createCollectable('database',25, 580, 10,15
 
     
     // TODO 3
@@ -59,7 +60,7 @@ createPlatform(290,371,30,15)
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
+createCannon('Top'25,0)
 
 
     
